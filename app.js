c3b6f07cc288fbd137c4b6a4fb5f7239cbf554eb
spailasoft7 <spaila.soft7@gmail.com>
2026-05-29 15:27:26 +0200

    Enhance login function with session storage and redirect
    Refactor login function to save session and user data in localStorage, and add a delay before redirecting to the dashboard.

--- app.js
+++ app.js
@@ -23,8 +23,11 @@ async function signup(){
 // LOGIN
 async function login(){
 
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
+  const email =
+    document.getElementById("email").value;
+
+  const password =
+    document.getElementById("password").value;
 
   const { data, error } =
     await supabaseClient.auth.signInWithPassword({
@@ -33,10 +36,30 @@ async function login(){
     });
 
   if(error){
+
     msg.innerText = error.message;
     return;
   }
 
-  // SUCCESS → go to app
-  window.location.href = "dashboard.html";
+  // SAVE SESSION
+  localStorage.setItem(
+    "spaila_session",
+    JSON.stringify(data.session)
+  );
+
+  // SAVE USER
+  localStorage.setItem(
+    "spaila_user",
+    JSON.stringify(data.user)
+  );
+
+  msg.innerText = "Login successful!";
+
+  // REDIRECT
+  setTimeout(() => {
+
+    window.location.href =
+      "dashboard.html";
+
+  }, 1000);
 }
